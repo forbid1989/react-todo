e1d12adaea3067c58e8526fc926c2393fd2fb713
ASIAPACIFIC\chengca <carl-meyrick.cheng@hpe.com>
2017-03-14 00:12:03 +0800

    Add filter functionality

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -19,5 +19,35 @@ module.exports = {
 
     return $.isArray(todos) ? todos: [];
 
+  },
+  filterTodos: function(todos, showCompleted, searchText) {
+    var filteredTodos = todos;
+
+    // Filter by showCompleted
+    filteredTodos = filteredTodos.filter((todo) => {
+      return !todo.completed || showCompleted;
+    });
+    // Filter by searchText
+    filteredTodos = filteredTodos.filter((todo) => {
+      if (searchText.length === 0) {
+        return todo;
+      } else {
+        return todo.text.toLowerCase().indexOf(searchText.toLowerCase()) > -1;
+      }
+    });
+
+    // Sort todos with non-completed first
+    filteredTodos.sort((a, b) => {
+      if (!a.completed && b.completed){
+        return -1;
+      } else if (a.completed && !b.completed) {
+        return 1;
+      } else {
+        return 0;
+      }
+
+    });
+
+    return filteredTodos;
   }
 };
